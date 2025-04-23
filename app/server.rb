049619d279bca2322e606ce5321b72e7542007d9
Jake Bills <jakebills1@gmail.com>
2025-04-23 10:47:33 -0600

    refactors request matching to case and reads Content-Encoding header

--- app/server.rb
+++ app/server.rb
@@ -18,33 +18,41 @@ class Request
   end
 end
 class Response
-  attr_accessor :version, :status, :body, :content_type
+  VALID_ENCODINGS = ['gzip'].freeze
+  attr_accessor :version, :status, :body, :content_type, :content_encoding
 
   def initialize
     @content_type = 'text/plain'
   end
+
+
   def self.from_request(request)
     response = new
     response.version = request.version
-    if request.target == '/'
+    case [request.verb, parse_path(request.target)]
+    when %w[GET /]
       response.status = '200 OK'
-    elsif request.target.start_with?('/echo')
+    when %w[GET /echo]
+      request_encoding = request.headers['Accept-Encoding']
+      if VALID_ENCODINGS.include? request_encoding
+        response.content_encoding = request_encoding
+      end
       response.body = request.target.split('/').last
       response.status = '200 OK'
-    elsif request.target.start_with?('/user-agent')
+    when %w[GET /user-agent]
       response.body = request.headers['User-Agent']
       response.status = '200 OK'
-    elsif request.target.start_with?('/files') && request.verb == 'GET'
-      response.content_type = 'application/octet-stream'
+    when %w[GET /files]
       file_path = OPTIONS['file_dir'] + request.target.split('/').last
       if File.exist?(file_path)
         file = File.read(file_path)
+        response.content_type = 'application/octet-stream'
         response.body = file
         response.status = '200 OK'
       else
         response.status = '404 Not Found'
       end
-    elsif request.target.start_with?('/files') && request.verb == 'POST'
+    when %w[POST /files]
       file_path = OPTIONS['file_dir'] + request.target.split('/').last
       File.write(file_path, request.body)
       response.status = '201 Created'
@@ -54,13 +62,34 @@ class Response
     response
   end
 
+  def write_headers
+    headers = ''
+    headers += "Content-Type: #{content_type}\r\n" if content_type
+    headers += "Content-Length: #{body.bytesize}\r\n" if body
+    headers += "Content-Encoding: #{content_encoding}\r\n" if content_encoding
+    headers
+  end
+
+  def write_body
+    return '' unless body
+
+    body
+  end
+
+  def write_request_line
+    "#{version} #{status}"
+  end
 
   def to_s
-    if body
-      "#{version} #{status}\r\nContent-Type: #{content_type}\r\nContent-Length: #{body.bytesize}\r\n\r\n#{body}"
-    else
-      "#{version} #{status}\r\n\r\n"
-    end
+    "#{write_request_line}\r\n#{write_headers}\r\n#{write_body}"
+  end
+
+  # returns first path segment for matching purposes
+  def self.parse_path(path)
+    idx_of_second_path_segment = path.index('/', 1)
+    return path unless idx_of_second_path_segment
+
+    path[0, idx_of_second_path_segment]
   end
 end
 
